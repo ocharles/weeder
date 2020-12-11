0ce2f1a2bc5f2e844211226f2232fcb91b3c4bad
Ollie Charles <ollie@ocharles.org.uk>
2020-12-11 09:10:49 +0000

    Fix CI (#55)
    This commit bumps the cachix-action and nix-install-action. We now have to specify a nixpkgs channel, and choosing 19.09 has needed a few other packages to be overriden.

--- nix/haskell/dhall.nix
+++ nix/haskell/dhall.nix
@@ -2,36 +2,38 @@
 , base, bytestring, case-insensitive, cborg, cborg-json, containers
 , contravariant, cryptonite, data-fix, deepseq, Diff, directory
 , doctest, dotgen, either, exceptions, filepath, foldl, gauge
-, generic-random, hashable, haskeline, http-client, http-client-tls
-, http-types, lens-family-core, megaparsec, memory, mockery, mtl
-, network-uri, optparse-applicative, parser-combinators, parsers
-, pretty-simple, prettyprinter, prettyprinter-ansi-terminal
-, profunctors, QuickCheck, quickcheck-instances, repline
-, scientific, semigroups, serialise, special-values, spoon, stdenv
-, tasty, tasty-expected-failure, tasty-hunit, tasty-quickcheck
-, template-haskell, text, th-lift-instances, transformers
+, generic-random, half, hashable, haskeline, http-client
+, http-client-tls, http-types, lens-family-core, megaparsec, memory
+, mmorph, mockery, mtl, network-uri, optparse-applicative
+, parser-combinators, parsers, pretty-simple, prettyprinter
+, prettyprinter-ansi-terminal, profunctors, QuickCheck
+, quickcheck-instances, repline, scientific, semigroups, serialise
+, special-values, spoon, stdenv, tasty, tasty-expected-failure
+, tasty-hunit, tasty-quickcheck, template-haskell, text
+, text-manipulate, th-lift-instances, transformers
 , transformers-compat, turtle, unordered-containers, uri-encode
 , vector
 }:
 mkDerivation {
   pname = "dhall";
-  version = "1.30.0";
-  sha256 = "f2be9599ddd88602c1577b0ca57849c9827c9e700e105102cecc17c56b7c4a81";
-  revision = "1";
-  editedCabalFile = "1pazhb3h1rabb80wxh29k5yfp915zqp1gmhcv4mx7ibzv9zw7miq";
+  version = "1.34.0";
+  sha256 = "0dbc61611d465f744aec13fd3114a9d75bbaa434f1aaa3de7e49c385d9fe1b67";
+  revision = "2";
+  editedCabalFile = "1gvfcizp3blqas5ccgnqmahwq26xwd23kqh1vc9712agq7384z98";
   isLibrary = true;
   isExecutable = true;
+  enableSeparateDataOutput = true;
   libraryHaskellDepends = [
     aeson aeson-pretty ansi-terminal atomic-write base bytestring
     case-insensitive cborg cborg-json containers contravariant
     cryptonite data-fix deepseq Diff directory dotgen either exceptions
-    filepath hashable haskeline http-client http-client-tls http-types
-    lens-family-core megaparsec memory mtl network-uri
-    optparse-applicative parser-combinators parsers pretty-simple
-    prettyprinter prettyprinter-ansi-terminal profunctors repline
-    scientific serialise template-haskell text th-lift-instances
-    transformers transformers-compat unordered-containers uri-encode
-    vector
+    filepath half hashable haskeline http-client http-client-tls
+    http-types lens-family-core megaparsec memory mmorph mtl
+    network-uri optparse-applicative parser-combinators parsers
+    pretty-simple prettyprinter prettyprinter-ansi-terminal profunctors
+    repline scientific serialise template-haskell text text-manipulate
+    th-lift-instances transformers transformers-compat
+    unordered-containers uri-encode vector
   ];
   executableHaskellDepends = [ base ];
   testHaskellDepends = [
@@ -39,8 +41,9 @@ mkDerivation {
     either filepath foldl generic-random lens-family-core megaparsec
     mockery prettyprinter QuickCheck quickcheck-instances scientific
     semigroups serialise special-values spoon tasty
-    tasty-expected-failure tasty-hunit tasty-quickcheck text
-    transformers turtle unordered-containers vector
+    tasty-expected-failure tasty-hunit tasty-quickcheck
+    template-haskell text transformers turtle unordered-containers
+    vector
   ];
   benchmarkHaskellDepends = [
     base bytestring containers directory gauge serialise text
