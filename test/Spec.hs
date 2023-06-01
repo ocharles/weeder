12ed3e3c4afebba1cb80f66db05b489a0f4a18fa
ryndubei <114586905+ryndubei@users.noreply.github.com>
2023-06-01 13:51:23 +0100

    Export dotfiles of dependency graphs

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,17 +1,19 @@
-{-# LANGUAGE LambdaCase #-}
 import qualified Weeder.Config
 import qualified Weeder.Main
+import qualified Weeder
 import qualified Dhall
 import qualified Data.Text as T
 
+import Algebra.Graph.Export.Dot
+import GHC.Types.Name.Occurrence (occNameString)
 import System.Directory
 import System.FilePath
 import System.IO.Silently (hCapture_)
-import System.IO (stdout)
+import System.IO (stdout, stderr, hPrint)
 import Test.Hspec
 import Control.Monad (zipWithM_, when)
-import Control.Exception (handle)
-import System.Exit (ExitCode(..))
+import Control.Exception (IOException)
+import Control.Exception.Base (handle)
 
 main :: IO ()
 main = do
@@ -39,15 +41,17 @@ discoverIntegrationTests = do
   pure . map ("./test/Spec" </>) $ filter (".stdout" `isExtensionOf`) contents
 
 -- | Run weeder on the given directory for .hie files, returning stdout
+-- Also creates a dotfile containing the dependency graph as seen by Weeder
 integrationTestOutput :: FilePath -> IO String
 integrationTestOutput hieDirectory = hCapture_ [stdout] $ do
   isEmpty <- not . any (".hie" `isExtensionOf`) <$> listDirectory hieDirectory
   when isEmpty $ fail "No .hie files found in directory, this is probably unintended"
-  ignoreExit $
-    Dhall.input Weeder.Config.config (T.pack dhallFile) >>= 
-      Weeder.Main.mainWithConfig ".hie" [hieDirectory] True
-  where 
+  (_, analysis) <-
+    Dhall.input Weeder.Config.config (T.pack dhallFile)
+      >>= Weeder.Main.mainWithConfig' ".hie" [hieDirectory] True
+  let graph = Weeder.dependencyGraph analysis
+      graph' = export (defaultStyle (occNameString . Weeder.declOccName)) graph
+  handle (\e -> hPrint stderr (e :: IOException)) $ 
+    writeFile (hieDirectory <.> ".dot") graph'
+  where
     dhallFile = hieDirectory <.> ".dhall"
-
-ignoreExit :: IO () -> IO ()
-ignoreExit = handle (\case ExitFailure _ -> pure (); ExitSuccess -> pure ())
