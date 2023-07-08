3e3f764e2d09d4823cd8c46e424cf695d6814d52
Ollie Charles <ollie@ocharles.org.uk>
2023-07-08 17:21:57 +0100

    Switch to Nix Flakes (#129)

---
+++ flake.nix
@@ -0,0 +1,58 @@
+{
+  description = "weeder";
+
+  nixConfig = {
+    extra-substituters = [
+      "https://weeder.cachix.org"
+      "https://cache.iog.io"
+    ];
+    extra-trusted-public-keys = [
+      "weeder.cachix.org-1:RdUmJalaHD50fGg3bNqsOGmGzRRqTIacmPBu/Xtjl3I="
+      "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+    ];
+  };
+
+  inputs = {
+    flake-utils.url = github:numtide/flake-utils;
+    haskellNix.url = github:input-output-hk/haskell.nix;
+    nixpkgs.follows = "haskellNix/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, haskellNix }:
+    flake-utils.lib.eachSystem ["x86_64-linux"] (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          inherit (haskellNix) config;
+          overlays = [ haskellNix.overlay ];
+        };
+
+        weeder = pkgs.haskell-nix.project {
+          compiler-nix-name = "ghc961";
+
+          cabalProjectLocal = builtins.readFile ./cabal.project.haskell-nix;
+
+          src = pkgs.haskell-nix.haskellLib.cleanGit {
+            name = "weeder";
+            src = ./.;
+          }; 
+
+          modules = [
+            {
+              reinstallableLibGhc = false;
+            }
+          ];
+        };
+      in
+      {
+        packages.default = weeder.hsPkgs.weeder.components.library;
+
+        devShells.default = weeder.shellFor {
+          withHoogle = false;
+          tools = { cabal = "latest"; };
+          exactDeps = false;
+          buildInputs = [ pkgs.changie ];
+        };
+      }
+    );
+}
