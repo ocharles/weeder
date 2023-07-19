99f9cfae7178be7e968a3cc4b80ffde968a7e334
ryndubei <114586905+ryndubei@users.noreply.github.com>
2023-07-19 17:23:16 +0100

    Type alias GADT test

---
+++ test/Spec/TypeAliasGADT/TypeAliasGADT.hs
@@ -0,0 +1,14 @@
+{-# LANGUAGE GADTs #-}
+
+module Spec.TypeAliasGADT.TypeAliasGADT where
+
+-- This is a false positive when the GADT language extension 
+-- is enabled and we do not consider type signatures
+type Secret = String
+
+data A = MkA String Int
+
+root :: Secret -> Int -> Secret
+root secret a =
+  let _params = MkA mempty a
+   in secret
